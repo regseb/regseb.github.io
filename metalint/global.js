baacfe898a072c4d7b357e5797e22c2f997a4318
Sébastien Règne <regseb@gmail.com>
2016-08-29 19:33:25 +0200

    Redesigner le site de Metalint.

--- metalint/global.js
+++ metalint/global.js
@@ -2,33 +2,51 @@
 
 "use strict";
 
-var $headers = $("h2");
+const $headers = $("h2");
 // Créer le sommaire de gauche.
 $headers.each(function () {
-    var $a = $("<a>").attr("href", "#" + $(this).attr("id"))
+    let $a = $("<a>").attr("href", "#" + $(this).attr("id"))
                      .text($(this).text());
     $("aside ul").append($("<li>").html($a));
 });
-var lastId;
-var topMenuHeight = $("aside").outerHeight();
-var $contents = $("aside a");
+let lastId;
+let topMenuHeight = $("aside").outerHeight();
+let $contents = $("aside a");
 $("aside li:first").addClass("active");
 
-var menuTop = $("aside").offset().top;
+let menuTop = 0;
+if (0 !== $("aside").length) {
+    menuTop = $("aside").offset().top;
+}
 $(window).scroll(function () {
+    if ($(this).scrollTop() >= $("nav").height()) {
+        $("nav > div").addClass("show");
+        $("h1, cite").addClass("hide");
+    } else {
+        $("nav > div").removeClass("show");
+        $("h1, cite").removeClass("hide");
+    }
+    if ($(this).scrollTop() >= $("header").offset().top +
+                                                         $("header").height()) {
+        $("nav").css("box-shadow", "0 2px 5px rgba(0, 0, 0, 0.26)");
+    } else {
+        $("nav").css("box-shadow", "none");
+    }
+
+
     if (menuTop < $(window).scrollTop()) {
         $("aside").css({ "position": "fixed" });
     } else {
         $("aside").css({ "position": "static" });
     }
 
-    var id = null;
+    let id = null;
     // Si l'utilisateur est en bas de la page.
     if ($(this).scrollTop() + $(this).height() === $(document).height()) {
         id = $("h2:last").attr("id");
     } else {
         // Get container scroll position
-        var fromTop = $(this).scrollTop() + topMenuHeight;
+        let fromTop = $(this).scrollTop() + topMenuHeight;
 
         // Rechercher la section courante.
         $headers.each(function () {
@@ -50,10 +68,10 @@ $(window).scroll(function () {
 }).scroll();
 
 // Gérer les changements d'onglet.
-for (var tab of document.querySelectorAll(".tabs > ul li")) {
+for (let tab of document.querySelectorAll(".tabs > ul li")) {
     tab.onclick = function () {
-        var index;
-        for (var i = 0; i < this.parentElement.children.length; ++i) {
+        let index;
+        for (let i = 0; i < this.parentElement.children.length; ++i) {
             if (this === this.parentElement.children[i]) {
                 index = i + 1;
                 this.className = "active";
@@ -62,8 +80,8 @@ for (var tab of document.querySelectorAll(".tabs > ul li")) {
             }
         }
 
-        var contents = this.parentElement.parentElement.children;
-        for (var i = 1; i < contents.length; ++i) {
+        let contents = this.parentElement.parentElement.children;
+        for (let i = 1; i < contents.length; ++i) {
             // Afficher le contenu de l'onglet sélectionné.
             if (index === i) {
                 contents[i].style.display = "block";
@@ -75,6 +93,6 @@ for (var tab of document.querySelectorAll(".tabs > ul li")) {
     }; // onclick()
 }
 // Afficher le contenu du premier onglet.
-for (var first of document.querySelectorAll(".tabs > ul li:first-child")) {
+for (let first of document.querySelectorAll(".tabs > ul li:first-child")) {
     first.click();
 }
